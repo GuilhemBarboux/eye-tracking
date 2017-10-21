98fb48112dcc19ea600e0405adfb5aa1c48b7e89
Guilhem Barboux <guilhembarboux@gmail.com>
2017-10-22 00:08:02 +0200

    Update tests with clmTrackr

--- site/assets/scripts/utils.js
+++ site/assets/scripts/utils.js
@@ -11,3 +11,46 @@ function webcam(success, failure) {
         alert("Your browser does not seem to support getUserMedia, using a fallback video instead.");
     }
 }
+
+var compatibility = (function() {
+    var lastTime = 0,
+
+        URL = window.URL || window.webkitURL,
+
+        requestAnimationFrame = function(callback, element) {
+            var requestAnimationFrame =
+                window.requestAnimationFrame		||
+                window.webkitRequestAnimationFrame	||
+                window.mozRequestAnimationFrame		||
+                window.oRequestAnimationFrame		||
+                function(callback, element) {
+                    var currTime = new Date().getTime();
+                    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                    var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    }, timeToCall);
+                    lastTime = currTime + timeToCall;
+                    return id;
+                };
+
+            return requestAnimationFrame.call(window, callback, element);
+        },
+
+        getUserMedia = function(options, success, error) {
+            var getUserMedia =
+                window.navigator.getUserMedia ||
+                window.navigator.mozGetUserMedia ||
+                window.navigator.webkitGetUserMedia ||
+                function(options, success, error) {
+                    error();
+                };
+
+            return getUserMedia.call(window.navigator, options, success, error);
+        };
+
+    return {
+        URL: URL,
+        requestAnimationFrame: requestAnimationFrame,
+        getUserMedia: getUserMedia
+    };
+})();
